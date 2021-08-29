aa896761da823f809fae513b22ba5ee695cf06c7
Yinglei Xu <yingle@ad.unc.edu>
2021-08-29 19:45:46 -0400

    Progress on Exercise1

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,6 @@
-# TODO: Write docstring here
-
-# TODO: Initialize __author__ variable here
-
-# TODO: Implement your program logic here
+"""My hype machine program for  COMP110."""
+__author__ = "730529273"
+name: str = input("What is your name? ")
+print(str(name) + " is smart.")
+print("Nice to meet you, " + str(name) + "!")
+print("I think, " + str(name) + ", what you says is correct!")
---
+++ exercises/ex01/relational_operators.py
@@ -0,0 +1,10 @@
+"""My relational operators program for COMP110."""
+__author__ = "730529273"
+left_hand_side: str = input("What is Left-hand side? ")
+right_hand_side: str = input("What is Right-hand side? ")
+print("Left-hand side: " + left_hand_side)
+print("Right-hand side: " + right_hand_side)
+print(str(left_hand_side) + " < " + str(right_hand_side) + " is " + str(int(left_hand_side) < int(right_hand_side)))
+print(str(left_hand_side) + " >= " + str(right_hand_side) + " is " + str(int(left_hand_side) >= int(right_hand_side)))
+print(str(left_hand_side) + " == " + str(right_hand_side) + " is " + str(int(left_hand_side) == int(right_hand_side)))
+print(str(left_hand_side) + " != " + str(right_hand_side) + " is " + str(int(left_hand_side) != int(right_hand_side)))
