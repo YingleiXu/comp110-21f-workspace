86a90fda6c647dbf45857937cd3ab3669945a23d
Yinglei Xu <yingle@ad.unc.edu>
2021-09-26 16:42:06 -0400

    ex04 progress 1

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,31 +1,19 @@
-
 """List utility functions."""
 __author__ = "730529273"
-
-
-def main() -> None:
-    """Entrypoint of program."""
-    numbers: list[int] = [1, 2, 3]
-    print(all(numbers, 1))
-    print(all([1, 1, 1], 2))
-    print(all(numbers, 2))
-    print(is_equal([1, 2, 3], [1, 2, 3]))
-    print(is_equal([1, 1, 0], [1, 0, 1]))
-    print(max([1, 2, 3]))
-    print(max([100, 99, 98]))
-    print(max([]))
 
 
 def all(numbers: list[int], b: int) -> bool:
     """Iff b is found in the numbers, return true."""
     # Move through each int within the list.
     i: int = 0
+    if len(numbers) == 0:
+        return False
     while i < len(numbers):
         item: int = numbers[i]
-        if item == b:
-            return True
-            i += 1
-    return False
+        if item != b:
+            return False
+        i += 1
+    return True
 
 
 def is_equal(list_1: list[int], list_2: list[int]) -> bool:
@@ -37,17 +25,24 @@ def is_equal(list_1: list[int], list_2: list[int]) -> bool:
 
 def max(a: list[int]) -> int:
     """This is a function which returns the maximun of a list."""
-    counter: int = 0
-    maximun: int = a[0]
     if len(a) == 0:
         raise ValueError("max() arg is an empty List")
+    else:
+        counter: int = 0
+        maximun: int = a[0]
+        while counter < len(a) - 1:
+            if maximun <= a[counter + 1]:
+                maximun = a[counter + 1]
+            counter += 1
+        return maximun
+    
 
-    while counter < len(a) - 1:
-        if maximun <= a[counter + 1]:
-            maximun = a[counter + 1]
-        counter += 1
-    return maximun
-
- 
-if __name__ == "__main__":
-    main()
+print(all([1, 2, 3], 1))
+print(all([1, 1, 1], 2))
+print(all([1, 2, 3], 2))
+print(is_equal([1, 2, 3], [1, 2, 3]))
+print(is_equal([1, 1, 0], [1, 0, 1]))
+print(is_equal([], [1, 2, 3]))
+print(max([1, 2, 3]))
+print(max([100, 99, 98]))
+print(max([]))
