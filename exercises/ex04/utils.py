0afa4ab03b5360a0964690808a8a9ee515a9d52c
Yinglei Xu <yingle@ad.unc.edu>
2021-09-26 15:42:35 -0400

    ex04 progress

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,3 +1,4 @@
+
 """List utility functions."""
 __author__ = "730529273"
 
@@ -6,12 +7,17 @@ def main() -> None:
     """Entrypoint of program."""
     numbers: list[int] = [1, 2, 3]
     print(all(numbers, 1))
+    print(all([1, 1, 1], 2))
+    print(all(numbers, 2))
     print(is_equal([1, 2, 3], [1, 2, 3]))
+    print(is_equal([1, 1, 0], [1, 0, 1]))
     print(max([1, 2, 3]))
+    print(max([100, 99, 98]))
+    print(max([]))
 
 
 def all(numbers: list[int], b: int) -> bool:
-    """Return True iff b is found in all()."""
+    """Iff b is found in the numbers, return true."""
     # Move through each int within the list.
     i: int = 0
     while i < len(numbers):
@@ -30,6 +36,7 @@ def is_equal(list_1: list[int], list_2: list[int]) -> bool:
 
 
 def max(a: list[int]) -> int:
+    """This is a function which returns the maximun of a list."""
     counter: int = 0
     maximun: int = a[0]
     if len(a) == 0:
