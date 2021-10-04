3fab750a290cb545289caa242bb8badbe0085942
Yinglei Xu <yingle@ad.unc.edu>
2021-10-04 17:07:45 -0400

    test exercise

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,53 @@
 """List utility functions part 2."""
-
-__author__ = "123456789"
+__author__ = "730529273"
 
 # Define your functions below
+
+
+def only_evens(xs: list[int]) -> list[int]:
+    """Return even numbers only."""
+    i: int = 0
+    xs_even: list[int] = []
+    if len(xs) == 0:
+        return []
+    else:
+        while i < len(xs):
+            if xs[i] % 2 == 0:
+                xs_even.append(xs[i])
+            i += 1
+    return xs_even
+
+
+def sub(a: list[int], b: int, c: int) -> list[int]:
+    """Generate a list which is a subset of the given list, between the specified start index and the end index - 1."""
+    list_returned: list[int] = []
+    while len(a_list) == 0:
+        if b > len(a_list) or c <= 0:
+            return []
+    if b < 0:
+        list_returned.append(a_list[0])
+    else:
+        list_returned.append(a_list[b])
+    if c > len(a_list):
+        list_returned.append(a_list[len(a_list) - 1])
+    else:
+        list_returned.append(a_list[c - 1])
+    return list_returned
+
+
+def concat(list_1: list[int], list_2: list[int]) -> list[int]:
+    """Concat two lists."""
+    for number in list_2:
+        list_1.append(number)
+    return list_1
+
+
+print(only_evens([1, 2, 3]))
+print(only_evens([1, 5, 3]))
+print(only_evens([4, 4, 4]))
+a_list: list[int] = [1, 2, 3, 4]
+print(sub(a_list, -1, 5))
+number: int
+list_input_1: list[int] = []
+list_input_2: list[int] = [5, 6, 7, 8]
+print(concat(list_input_1, list_input_2))
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,59 @@
 """Unit tests for list utility functions."""
+__author__ = "730529273"
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+from exercises.ex05.utils import only_evens, sub, concat
+
+
+def test_only_evens_empty() -> None:
+    xs: list[int] = []
+    assert only_evens(xs) == []
+
+
+def test_only_evens_single() -> None:
+    xs: list[int] = [1, 2, 3]
+    assert only_evens(xs) == [2]
+
+
+def test_only_evens_many() -> None:
+    xs: list[int] = [1, 2, 3, 4]
+    assert only_evens(xs) == [2, 4]
+
+
+def test_sub_special() -> None:
+    xs: list[int] = [1, 2, 3, 4]
+    b: int = -1
+    c: int = 5
+    assert sub(xs, b, c) == [1, 4]
+
+
+def test_sub_normal_1() -> None:
+    xs: list[int] = [1, 2, 3, 4]
+    b: int = 1
+    c: int = 3
+    assert sub(xs, b, c) == [2, 3]
+
+
+def test_sub_normal_2() -> None:
+    xs: list[int] = [10, 20, 30, 40, 50]
+    b: int = 2
+    c: int = 4
+    assert sub(xs, b, c) == [30, 40]
+
+
+def test_concat_empty() -> None:
+    list_1: list[int] = []
+    list_2: list[int] = [1, 2, 3, 4]
+    assert concat(list_1, list_2) == [1, 2, 3, 4]
+
+
+def test_concat_normal_1() -> None:
+    list_1: list[int] = [1, 2, 3, 4]
+    list_2: list[int] = [5, 6, 7, 8]
+    assert concat(list_1, list_2) == [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def test_concat_normal_2() -> None:
+    list_1: list[int] = [4, 5, 6, 7, 8]
+    list_2: list[int] = [2, 3, 4, 5]
+    assert concat(list_1, list_2) == [4, 5, 6, 7, 8, 2, 3, 4, 5]
